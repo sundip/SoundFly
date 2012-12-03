d6ce17954f4e3170279e77df594ce3f72065bec8
sundip <mail@sundip.me>
2012-12-03 14:55:50 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SoundFly
+
+SoundFly is application used to record and share audio samples 7 seconds at a time.
+Created by Sundip Patel (@TheSundip)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundFly::Application.config.secret_token = 'ba5f42551ea11adde2c6b7725b32bf8f17015e11b29841df189d569a0b71c64424c6aae05bf531b5626a672a27346bf5fe82cf41550910d7e9d01f432ebdf8de'
