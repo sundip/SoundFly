3d66b551e504ed4b3d5d21c6706d9842778e8a34
sundip <mail@sundip.me>
2012-12-07 15:39:15 -0600

    Finish User SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SoundFly::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
   
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,31 +2,59 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
-     before { visit root_path } 
+    before { visit root_path }
 
-      it { should have_selector('h1', text: 'SoundFly') }
-      it { should have_selector 'title',
-                          text: "SoundFly" }
-      it { should_not have_selector 'title', text: '| Home' }
+    it { should have_selector('h1', text: 'SoundFly') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
-  
+
+
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "SoundFly | About Us")
-    end
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
-  
-end
 
-#root_path
-#about_path
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'h1', text: 'SoundFly'
+  end
+end
